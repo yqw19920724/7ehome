4c0a78af5f24be1cf37bb848e7c35121f305248e
yqw19920724 <986590953@qq.com>
2018-05-10 19:53:02 +0800

    建立路由libs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var users = require('./libs/users')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/users', users);
 
 module.exports = router;
