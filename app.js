81c75f4f33334fbbd794c00c8e18f5a8510a48be
yqw19920724 <986590953@qq.com>
2018-05-14 21:38:12 +0800

    修改请求头 设置不缓存数据

--- app.js
+++ app.js
@@ -26,11 +26,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");     //设置跨域
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1');
     res.header("Content-Type", "application/json;charset=utf-8");
+    res.header("Cache-control", "no-cache, no-store, must-revalidate");   //设置请求不缓存
     next();
 });
 
