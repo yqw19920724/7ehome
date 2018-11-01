b317a41c5af8e0f79a4faaceca0ea1747b51bd16
chenfeng <chenfeng01@shejijia.com.cn>
2018-11-01 15:38:04 +0800

    调整代码格式

--- libs/controller/goodCtrl.js
+++ libs/controller/goodCtrl.js
@@ -1,14 +1,13 @@
 const goodService = require('../service/goodService');
+const uploadCtrl = require('./uploadCtrl');
 const common = require('../common/common');
 
 //获取商品
 exports.getGoodsList = (req, res) => {
     const params = {limit: req.query.limit, page: req.query.page,};
-    goodService.getGoodsList(params).then(data => {
-        return common.handleCtrlData(data, res);
-    }).catch(err => {
-        return common.handleCtrlData(err, res);
-    })
+    goodService.getGoodsList(params)
+    .then(data => common.handleCtrlData(data, res))
+    .catch(err => common.handleCtrlData(err, res))
 };
 
 //修改商品信息
@@ -18,11 +17,9 @@ exports.updateGood = (req, res) => {
         return common.handleCtrlData({err: common.errorMsg.good.GOOD_ID_ERROR}, res);
     }
     const body = {price: req.body.price, name: req.body.name};
-    goodService.updateGood(goodId, body).then(good => {
-        return common.handleCtrlData(good, res);
-    }).catch(err => {
-        return common.handleCtrlData(err, res);
-    })
+    goodService.updateGood(goodId, body)
+    .then(good => common.handleCtrlData(good, res))
+    .catch(err => common.handleCtrlData(err, res))
 };
 
 //创建商品
@@ -32,11 +29,9 @@ exports.createGood =  (req, res) => {
     if(!name || !price) {
         return common.handleCtrlData({err: common.errorMsg.good.DATA_IS_INCOMPLETE}, res);
     }
-    goodService.createGood({name: name, price: price}).then(good => {
-        return common.handleCtrlData(good, res);
-    }).catch(err => {
-        return common.handleCtrlData(err, res);
-    })
+    goodService.createGood({name: name, price: price})
+    .then(good => common.handleCtrlData(good, res))
+    .catch(err => common.handleCtrlData(err, res))
 };
 
 //删除商品
@@ -45,11 +40,9 @@ exports.deleteGood =  (req, res) => {
     if(!goodId) {
         return common.handleCtrlData({err: common.errorMsg.good.GOOD_ID_ERROR}, res);
     }
-    goodService.deleteGood(goodId).then(good => {
-        return common.handleCtrlData(good, res);
-    }).catch(err => {
-        return common.handleCtrlData(err, res);
-    })
+    goodService.deleteGood(goodId)
+    .then(good => common.handleCtrlData(good, res))
+    .catch(err => common.handleCtrlData(err, res))
 };
 
 //加入购物车
@@ -62,11 +55,9 @@ exports.addCart = (req, res) => {
     if(!goodId) {
         return common.handleCtrlData({err: common.errorMsg.good.GOOD_ID_ERROR}, res);
     }
-    goodService.addCart({user, goodId}).then(good => {
-        return common.handleCtrlData(good, res);
-    }).catch(err => {
-        return common.handleCtrlData(err, res);
-    })
+    goodService.addCart({user, goodId})
+    .then(good => common.handleCtrlData(good, res))
+    .catch(err => common.handleCtrlData(err, res))
 };
 
 //取消购物车
@@ -79,11 +70,9 @@ exports.removeCart = (req, res) => {
     if(!goodId) {
         return common.handleCtrlData({err: common.errorMsg.good.GOOD_ID_ERROR}, res);
     }
-    goodService.removeCart({user, goodId}).then(good => {
-        return common.handleCtrlData(good, res);
-    }).catch(err => {
-        return common.handleCtrlData(err, res);
-    })
+    goodService.removeCart({user, goodId})
+    .then(good => common.handleCtrlData(good, res))
+    .catch(err => common.handleCtrlData(err, res))
 };
 
 //获取购物车
@@ -94,9 +83,7 @@ exports.getCart = (req, res) => {
     if(!user) {
         return common.handleCtrlData({err: common.errorMsg.user.PLEASE_LOGIN}, res);
     }
-    goodService.getCart({user, limit, page}).then(good => {
-        return common.handleCtrlData(good, res);
-    }).catch(err => {
-        return common.handleCtrlData(err, res);
-    })
+    goodService.getCart({user, limit, page})
+    .then(good => common.handleCtrlData(good, res))
+    .catch(err => common.handleCtrlData(err, res))
 }
